820c6ab233f62246a598ac2100d7fee80048402d
EladOvadia <elgd321@gmail.com>
2024-11-07 14:07:41 +0200

    added endpoint to retrieve intro video

--- src/app.js
+++ src/app.js
@@ -1,27 +1,29 @@
-
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-const config = require('./../config');
-const userRoutes = require('./routes/users');
-const productsRoutes = require('./routes/products');
-const authRoutes = require('./routes/auth');
-const supplierRoutes = require('./routes/supplier');
-const adminRoutes = require('./routes/admin');
-const { authenticateToken } = require('./middlewares/auth');
+const bodyParser = require("body-parser");
+const config = require("./../config");
+const userRoutes = require("./routes/users");
+const productsRoutes = require("./routes/products");
+const authRoutes = require("./routes/auth");
+const supplierRoutes = require("./routes/supplier");
+const adminRoutes = require("./routes/admin");
+const { authenticateToken } = require("./middlewares/auth");
 
 const app = express();
-const baseRoute = `${config.app.baseName}/api`
+const baseRoute = `${config.app.baseName}/api`;
 
 // Increase the size limit for JSON and URL-encoded data
-app.use(express.json({ limit: '5mb' })); // Set limit as per your need (e.g., 10MB)
-app.use(express.urlencoded({ limit: '5mb', extended: true })); // For URL-encoded data
+app.use(express.json({ limit: "5mb" })); // Set limit as per your need (e.g., 10MB)
+app.use(express.urlencoded({ limit: "5mb", extended: true })); // For URL-encoded data
 
 // Middleware setup
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
+app.get(`${baseRoute}/intro`, async (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "assets", "intro.mp4"));
+});
 
 // Exclude auth routes from authentication
 app.use(`${baseRoute}/token`, authRoutes);
@@ -29,7 +31,7 @@ app.use(`${baseRoute}/token`, authRoutes);
 // Apply middleware to all routes except specifics
 app.use((req, res, next) => {
   if (req.path === `${baseRoute}/user/register`) {
-      return next(); // Skip middleware for this route
+    return next(); // Skip middleware for this route
   }
   authenticateToken(req, res, next); // Apply middleware for all other routes
 });
@@ -40,11 +42,7 @@ app.use(`${baseRoute}/products`, productsRoutes);
 app.use(`${baseRoute}/supplier`, supplierRoutes);
 app.use(`${baseRoute}/admin`, adminRoutes);
 
-
-
-
 // app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from 'public' folder
-
 
 app.use(cors({ origin: "*" }));
 app.use((req, res, next) => {
@@ -70,24 +68,19 @@ app.get(`/image-repo/:img`, async (req, res) => {
   const { img } = req.params;
   console.log(img);
 
-  res.sendFile(path.join(__dirname, "public", 'assets', img));
+  res.sendFile(path.join(__dirname, "public", "assets", img));
 });
 
 app.get(`${config.app.baseName}/404`, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-
-
 app.get(config.app.baseName, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-
 // todo move client to dist under src :)
 // app.use(express.static(path.join(__dirname, "dist")));
-
-
 
 app.get(`${config.app.baseName}/*`, (req, res, next) => {
   const requestPath = req.path.replace(config.app.baseName, "");
@@ -104,19 +97,14 @@ app.get(`${config.app.baseName}/*`, (req, res, next) => {
   }
 });
 
-
 app.use((req, res) => {
   res.redirect(`${config.app.baseName}/404`);
 });
 
-
-
 // Error handling middleware (optional) -- move to middlewares
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
-
-
 
 module.exports = app;
