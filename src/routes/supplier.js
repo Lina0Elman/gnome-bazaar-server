d40a700bbb5b200bc9476fbdd56bd2705d42b71c
lina elman <linael2002@gmail.com>
2024-11-09 17:13:59 +0200

    removed unnecessry code

--- src/routes/supplier.js
+++ src/routes/supplier.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const supplierController = require('../controllers/suppliers');
-const {authenticateToken} = require('../middlewares/auth');
-
 
 
 router.get('/supplier-sales-info', supplierController.getSalesInfo);
